27ea2a27cde5718d43ce9ac674aa4bd9720b8847
Dhruvin2910 <dhruvinpatel321@gmail.com>
2025-08-31 18:25:38 +0530

    basic server setup and mongodb connection done

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
+const dotenv = require("dotenv");
+const PORT = 8000;
+
+//Load exnvironment from variables files
+dotenv.config();
+
+//connect with mongoDB
+require("./Config/db");
+
+app.listen(PORT, () => {
+    console.log(`Listining to Port ${PORT}`);
+})
+
+app.get("/test", (req, res) => {
+    res.write("Welcome to Nodejs server");
+    res.send();
+})
