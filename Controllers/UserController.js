803b3011701f77e7c19e388cbe8a25c3c7334717
Dhruvin2910 <dhruvinpatel321@gmail.com>
2025-09-02 01:33:46 +0530

    uploadAvatar functionality added

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,5 +1,6 @@
 const UserModel = require("../Models/User");
-
+const cloudinary = require("../Config/cloudinary");
+const streamifier = require("streamifier");
 
 const updateProfile = async (req, res) => {
     try{
@@ -43,11 +44,48 @@ const updateProfile = async (req, res) => {
             return res.status(404).json({ success:false, message:"User not found" });
         }
 
-        res.status(200).json({ success:true, user: updateUser });
+        res.status(200).json({ success:true, user: updateUser }); 
     }catch(err) {
         console.error(err);
         return res.status(500).json({ success:false, message:"Server error" });
     }
 }
 
-module.exports = { updateProfile };
+const uploadAvatar = async (req, res) => {
+    try{
+        if(!req.file){
+            return res.status(404).json({ success:false, message:"No file uploaded" });
+        }
+
+        // Upload the file buffer directly to Cloudinary
+        const result = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream(
+               {folder: "avatars", width: 200, height: 200, crop: "fill"},
+               (error, result) => {
+                if(result) resolve(result);
+                else reject(error);
+               } 
+            );
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+
+        // Update user's avatar URL in MongoDB
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            req.user.id,
+            { avatar:result.secure_url },
+            { new:true }
+        );
+
+        res.status(200).json({ 
+            success:true, 
+            message:"Avatar uploaded successfully",
+            avatar: result.secure_url,
+            user: updatedUser
+        })
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({ success:false, message:"Server error" });
+    }
+}
+
+module.exports = { updateProfile, uploadAvatar };
--- Routes/User.js
+++ Routes/User.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { updateProfile } = require("../Controllers/UserController");
+const { updateProfile, uploadAvatar } = require("../Controllers/UserController");
 const userValidation = require("../Validators/UserValidation");
 const ensureAuthenticated = require("../Middleware/Auth");
+const multer = require("multer");
+
+// Memory storage: files stored in RAM as buffer
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.put("/profile", ensureAuthenticated, userValidation, updateProfile);
+router.post("/profile/avatar", ensureAuthenticated, upload.single("avatar"), uploadAvatar);
 
 module.exports = router;
