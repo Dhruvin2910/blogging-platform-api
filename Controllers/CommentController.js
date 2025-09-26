b09ca734f1ba849b819f2427be82f4d02d0b605f
Dhruvin2910 <dhruvinpatel321@gmail.com>
2025-09-26 17:36:13 +0530

    createNestedComment added

--- Controllers/CommentController.js
+++ Controllers/CommentController.js
@@ -5,7 +5,7 @@ const createComment = async (req, res) => {
   try {
     const { text } = req.body;
     const user = req.user;
-    const { postId } = req.params; 
+    const { postId } = req.params;
 
     if (!text) {
       return res
@@ -36,20 +36,77 @@ const createComment = async (req, res) => {
   }
 };
 
+// Create Nested Comment
+const createNestedComment = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const { postId, commentId } = req.params;
+    const user = req.user;
+
+    if (!text) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Comment cannot be empty" });
+    }
+
+    const post = await PostModel.findById(postId);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "POst not found" });
+    }
+
+    const parentComment = await post.comments.id(commentId);
+    if (!parentComment) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Parent comment not found" });
+    }
+
+    const newNestedComment = {
+      user: user.id,
+      comment: text,
+      createdAt: new Date(),
+    };
+
+    parentComment.replies.push(newNestedComment);
+
+    await post.save();
+
+    res.status(200).json({ success: true, newNestedComment });
+  } catch (err) {
+    console.error("Create Nested Comment Error: ", err);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
 // Get all comments for a post
 const getAllComment = async (req, res) => {
   try {
     const { postId } = req.params;
 
-    const post = await PostModel.findById(postId).populate("comments.user", "name email");
+    const post = await PostModel.findById(postId).populate(
+      "comments.user",
+      "name email"
+    ); 
     if (!post) {
       return res
         .status(404)
         .json({ success: false, message: "Post not found" });
     }
-    const sortedComments = post.comments.sort((a,b) => b.createdAt - a.createdAt);
+    const sortedComments = post.comments.sort(
+      (a, b) => b.createdAt - a.createdAt
+    );
 
-    res.status(200).json({ success: true, conut:sortedComments.length, comments: sortedComments });
+    res
+      .status(200)
+      .json({
+        success: true,
+        conut: sortedComments.length,
+        comments: sortedComments,
+      });
   } catch (err) {
     console.error(err);
     res.status(500).json({ success: false, message: "Internal Server Error" });
@@ -64,7 +121,9 @@ const deleteComment = async (req, res) => {
 
     const post = await PostModel.findById(postId);
     if (!post) {
-      return res.status(404).json({ success: false, message: "Post not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
     }
 
     const comment = post.comments.id(commentId);
@@ -78,7 +137,6 @@ const deleteComment = async (req, res) => {
     console.log(user.id.toString());
     console.log(comment.user.toString());
 
-
     // Authorization: admin, post author, or comment author
     if (
       user.role !== "admin" &&
@@ -87,18 +145,28 @@ const deleteComment = async (req, res) => {
     ) {
       return res
         .status(401)
-        .json({ success: false, message: "Not authorized to delete this comment" });
+        .json({
+          success: false,
+          message: "Not authorized to delete this comment",
+        });
     }
 
     // Remove comment
     comment.deleteOne();
     await post.save();
 
-    res.status(200).json({ success: true, message: "Comment deleted successfully" });
+    res
+      .status(200)
+      .json({ success: true, message: "Comment deleted successfully" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ success: false, message: "Server error" });
   }
 };
 
-module.exports = { createComment, getAllComment, deleteComment };
+module.exports = {
+  createComment,
+  createNestedComment,
+  getAllComment,
+  deleteComment,
+};
--- Routes/Comment.js
+++ Routes/Comment.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router({ mergeParams:true });
 
-const { createComment, deleteComment, getAllComment } = require("../Controllers/CommentController");
+const { createComment, createNestedComment, deleteComment, getAllComment } = require("../Controllers/CommentController");
 const ensureAuthenticated = require("../Middleware/Auth");
 
 // Protected Route
 router.get("/", ensureAuthenticated, getAllComment);
 router.post("/", ensureAuthenticated, createComment);
+router.post("/:commentId/reply", ensureAuthenticated, createNestedComment);
 router.delete("/:commentId", ensureAuthenticated, deleteComment);
  
 module.exports = router;
